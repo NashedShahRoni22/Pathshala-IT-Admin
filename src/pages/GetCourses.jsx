e47c19badc69a407659cb2858cf96915266d8540
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2023-11-07 23:05:17 +0600

    card design changed

--- src/pages/GetCourses.jsx
+++ src/pages/GetCourses.jsx
@@ -46,16 +46,17 @@ export default function GetCourses() {
       ) : (
         <div className="mt-5 grid gap-5 md:grid-cols-2 lg:grid-cols-4">
           {courses?.map((c, i) => (
-            <div key={i} className="shadow rounded">
-              <img src={c?.course_image} alt="" />
-              <div className="p-2 flex flex-col gap-2.5">
+            <div key={i} className="shadow rounded flex flex-col justify-between">
+              <img src={c?.course_image} className="w-[380px] h-[240px]" alt="" />
+              <div className="p-2">
                 <h5 className="text-xl font-semibold">{c?.name}</h5>
-                <p>{c?.course_overview?.slice(0, 100)}</p>
-                <div className="flex justify-between">
+                <p className="my-2.5">{c?.course_overview?.slice(0, 100)}</p>
+                
+              </div>
+              <div className="flex justify-between p-2">
                   <Button color="blue">View</Button>
                   <Button color="red">Delete</Button>
                 </div>
-              </div>
             </div>
           ))}{" "}
         </div>
